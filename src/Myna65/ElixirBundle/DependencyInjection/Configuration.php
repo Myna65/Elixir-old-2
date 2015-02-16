90ec5ecc517c1b6c5451fe3d4277037ad6df3c35
Meynaert Nathan <myna65@hotmail.be>
2015-02-16 17:58:44 +0100

    Fixed warnings

--- src/Myna65/ElixirBundle/DependencyInjection/Configuration.php
+++ src/Myna65/ElixirBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('myna65_elixir_relations');
+        //$rootNode = $treeBuilder->root('myna65_elixir_relations');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
